6ff5fb533157df2ecd9d04900e3b912c161a43b3
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2024-01-30 15:06:38 +0100

    add pure.css framework and update form views with it

--- app/views/login.php
+++ app/views/login.php
@@ -5,25 +5,46 @@ include_once 'components/header.php';
 
 ?>
 
-<h1>Connexion</h1>
-    <form action="login.inc.php" method="POST">
-        <label for="pseudo">Pseudo</label> <input type="text" id="pseudo" name="username" required>
-        <br/>
-        <label for="mdp">Mot de passe</label> <input type="password" id="mdp" name="password" required>
-        <div class="errlog">
-        <?php
-        if(isset($_GET["error"])){?>
-        <h4>Le nom d'utilisateur et/ou le mot de passe utilisé contient des erreurs.</h4>
-        <?php
-        }
-        ?>
+<?php
+$page_title = "Inscription" ;
+include_once 'components/header.php';
+?>
+<div class="content-wrapper">
+    <div class="content">
+        <div class="l-box-lrg pure-u-2 pure-u-md-1-5">
+            <div class="pure-g">
+                <div class=" pure-u-2-3">
+                    <img src="https://img.freepik.com/free-vector/sign-page-abstract-concept-illustration_335657-3875.jpg" class="img-form"/>
+                </div>
+                <div class="l-box-lg-3-4 pure-u-2-3">
+                    <form action="login.inc.php" method="POST">
+                        <h1>Connexion</h1>
+                        <label for="pseudo">Pseudo</label> <input type="text" id="pseudo" name="username" required>
+                        <br/>
+                        <label for="mdp">Mot de passe</label> <input type="password" id="mdp" name="password" required>
+                        <br/>
+                        <div class="errlog">
+                        <?php
+                        if(isset($_GET["error"])){?>
+                        <h4>Le nom d'utilisateur et/ou le mot de passe utilisé contient des erreurs.</h4>
+                        <?php
+                        }
+                        ?>
+                        </div>
+                        <div>
+                        <br/>
+                            <input type="reset" value="Réinitialiser" class="pure-button button-secondary"/> 
+                            <input type="submit" name="loginsubmit" value="Connexion" class="pure-button"/><br/>
+                            <a href="forgotpassword.php" class="apwd">Mot de passe oublié ?</a>
+                            <br/>
+                            <a href="signin.php" class="apwd">Pas encore inscrit ?</a>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
-        <input type="reset" value="réinitialiser"/> <input type="submit" name="loginsubmit" value="connexion"/>
-        <a href="forgotpassword.php" class="apwd">Mot de passe oublié ?</a>
-
-        <a href="signin.php" class="apwd">Pas encore inscrit ?</a>
-
-    </form>
+    </div>
 <?php
 include_once 'components/footer.php';
 ?>
--- app/views/signin.php
+++ app/views/signin.php
@@ -2,39 +2,56 @@
 $page_title = "Inscription" ;
 include_once 'components/header.php';
 ?>
-
-    <h1>Inscription</h1>
-    <form action="signin.inc.php" method="POST">
-    <label for="username">Nom d'utilisateur</label> 
-        <input type="text" id="username" name="username" required>
-        <br/>
-        <label for="email">Adresse email</label> 
-        <input type="email" id="email" name="email" required>
-        <br/>
-        <label for="mdp">Mot de passe</label> 
-        <input type="password" id="mdp" name="password" required>
-        <br/>
-        <label for="mdp">Répéter le mot de passe</label> 
-        <input type="password" id="mdp" name="rpassword" required>
-        <span class="errsignin">
-        <?php
-            if(isset($_GET['error'])){
-                if($_GET['error'] == 'nomatchpassword'){
-                    ?>
-                    <p>Les mots de passe entrés ne correspondent pas.</p>
-                    <?php
-                } 
-                if($_GET['error'] == 'existingID'){
-                    ?>
-                    <p>Le pseudo ou l'email est indisponible</p>
-                    <?php
-                }    
-            }
-        ?></span>
-        <br/>
-        <input type="reset" value="réinitialiser"/> 
-        <input type="submit" name="signinsubmit" value="s'enregistrer"/>
-        <a href="login.php" class="apwd">J'ai déjà un compte</a>
-    </form>
-</body>
-</html>
+<div class="content-wrapper">
+    <div class="content">
+        <div class="l-box-lrg pure-u-2 pure-u-md-1-5">
+            <div class="pure-g">
+                <div class="l-box-lg-1-4 pure-u-2-3">
+                    <form action="signin.inc.php" method="POST">
+                        <h1>Inscription</h1>
+                        <label for="username">Nom d'utilisateur</label> 
+                        <input type="text" id="username" name="username" required>
+                        <br/>
+                        <label for="email">Adresse email</label> 
+                        <input type="email" id="email" name="email" required>
+                        <br/>
+                        <label for="mdp">Mot de passe</label> 
+                        <input type="password" id="mdp" name="password" required>
+                        <br/>
+                        <label for="mdp">Répéter le mot de passe</label> 
+                        <input type="password" id="mdp" name="rpassword" required>
+                        <br/>
+                        <span class="errsignin">
+                            <?php
+                                if(isset($_GET['error'])){
+                                    if($_GET['error'] == 'nomatchpassword'){
+                                        ?>
+                                        <p>Les mots de passe entrés ne correspondent pas.</p>
+                                        <?php
+                                    } 
+                                    if($_GET['error'] == 'existingID'){
+                                        ?>
+                                        <p>Le pseudo ou l'email est indisponible</p>
+                                        <?php
+                                    }    
+                                }
+                            ?>
+                        </span>
+                        <br/>
+                        <input type="reset" value="Réinitialiser" class="pure-button button-secondary"/> 
+                        <input type="submit" name="signinsubmit" value="S'enregistrer" class="pure-button pure-button-primary"/>
+                        <br/>
+                        <div>
+                            <a href="login.php" class="apwd">J'ai déjà un compte</a>
+                        </div>
+                    </form>
+                </div>
+                <div class=" pure-u-2-3">
+                    <img src="https://img.freepik.com/free-vector/access-control-system-abstract-concept_335657-3180.jpg" class="img-form"/>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include_once 'components/footer.php';
+?>
