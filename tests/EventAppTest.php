7d72adc9c7a59a73b157f62ee713c5b4d0f0f03f
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2024-01-31 17:14:50 +0100

    add phpUnit and test file without content in test methods

---
+++ tests/EventAppTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    public function testSuccessfulRegistration()
+    {
+    }
+
+    public function testRegistrationWithEmailAlreadyUsed()
+    {
+    }
+
+    public function testSuccessfulLogin()
+    {
+    }
+
+    public function testSuccessfulLogout()
+    {
+    }
+
+    public function testAccessProtectedPagesWithoutLogin()
+    {
+    }
+
+    public function testCreateEvent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testEditEvent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDeleteEvent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testParticipateEvent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testWithdrawEvent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCheckIfUserParticipated()
+    {
+        $this->assertTrue(true);
+    }
+}
